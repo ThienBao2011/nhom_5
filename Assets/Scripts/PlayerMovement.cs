804b4c5a3ac4385b9deadb15769cbe8d73858e56
LapizLazulie <169118081+LapizLazulie@users.noreply.github.com>
2024-05-30 20:58:02 +0700

    playermoment2 (di chuyen nv)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,17 +5,26 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    [SerializeField] float moveSpeed = 5f;
+    private Rigidbody2D _rigidbody2D;
     Vector2 moveInput;
+    private Animator _animator;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+        FlipSprite();
+
     }
     void OnMove(InputValue value)
     {
@@ -33,5 +42,28 @@ public class PlayerMovement : MonoBehaviour
         Debug.Log(">>>>> Jump");
     }
 
+    // Dieu khien chuyen dong cua nv
+    void Run()
+    {
+        var moveVelocity = new Vector2(x:moveInput.x * moveSpeed, _rigidbody2D.velocity.y);
+        _rigidbody2D.velocity = moveVelocity;
+
+        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidbody2D.velocity.x) > Mathf.Epsilon;
+        _animator.SetBool(name: "isRunning", playerHasHorizontalSpeed);
+
+
+    }
+    //Abs: gia tri tuyet doi
+    //Sign: dau cua gia tri
+    //Epilon: gia tri nho nhat co the so sanh
+    //Xoay huong nv theo chieu chuyen dong
+    void FlipSprite()
+    {
+        bool playerHasHorizontalSpeed = Mathf.Abs(_rigidbody2D.velocity.x) > Mathf.Epsilon;
+        if (playerHasHorizontalSpeed)
+        {
+            transform.localScale = new Vector2(x: Mathf.Sign(_rigidbody2D.velocity.x), y: 1f);
+        }
+    }
 
 }
