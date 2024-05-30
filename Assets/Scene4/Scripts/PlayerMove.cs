7f990b42e8e7b1d756341d13af617b89d58275ca
Haiballad <hainvdps38602@gmail.com>
2024-05-30 19:25:44 +0700

    Move_Player

--- Assets/Scene4/Scripts/PlayerMove.cs
+++ Assets/Scene4/Scripts/PlayerMove.cs
@@ -5,26 +5,46 @@ using UnityEngine.InputSystem;
 
 public class PlayerMove : MonoBehaviour
 {
+    Rigidbody2D rb;
     Vector2 moveInput;
+    [SerializeField] float moveSpeed = 5f;
+    Animator ani;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+        ani = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Run();
+        Flip();
     }
     
     void OnMove(InputValue value)
     {
         moveInput = value.Get<Vector2>();
-        Debug.Log($"Move Input: {moveInput}");
+
     }
     void OnJump()
     {
-        Debug.Log(">>>> JUMP >>>>>");
+
+    }
+    void Run()
+    {
+        Vector2 moveVelocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
+        rb.velocity = moveVelocity;
+        bool soSanh = Mathf.Abs(moveInput.x) > Mathf.Epsilon;
+        ani.SetBool("isRunning", soSanh);
+    }
+    void Flip()
+    {
+        bool soSanh = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        if (soSanh)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
     }
 }
