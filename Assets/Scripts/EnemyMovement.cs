b6dd2b7f4426c910fad936fe9ff7fc541bdf6529
LapizLazulie <169118081+LapizLazulie@users.noreply.github.com>
2024-05-31 19:00:23 +0700

    Enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    private Rigidbody2D _rigidbody2D;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody2D= GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    // Update is called once per frame
+    void Update()
+    {
+        _rigidbody2D.velocity = new Vector2(moveSpeed, 0);
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        moveSpeed *= -1;
+        //xoay huong cua quai vat
+        transform.localScale = new Vector2(-(Mathf.Sign(_rigidbody2D.velocity.x)), 1f);
+    }
+}
