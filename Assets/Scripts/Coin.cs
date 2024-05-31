a0674cf696b324bae2574befc1f620cf85ddd0cc
LapizLazulie <169118081+LapizLazulie@users.noreply.github.com>
2024-06-01 00:16:26 +0700

    UI score live

--- Assets/Scripts/Coin.cs
+++ Assets/Scripts/Coin.cs
@@ -7,11 +7,18 @@ public class Coin : MonoBehaviour
     [SerializeField] float coinValue = 100;
     [SerializeField] AudioClip Coin_flip;
 
+    //Dong xu da duoc nhat chua
+    private bool isCollected = false;
+
     public void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isCollected)
         {
+            isCollected = true;
+            //tang diem
+            FindObjectOfType<GameController>().AddScore((int)coinValue);
             AudioSource.PlayClipAtPoint(Coin_flip, Camera.main.transform.position);
+            gameObject.SetActive(false);
             Destroy(gameObject);
         }
     }
