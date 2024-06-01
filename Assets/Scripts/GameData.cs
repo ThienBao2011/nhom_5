c0976aedf3abd3e780d182f729072c4fb329c45a
LapizLazulie <169118081+LapizLazulie@users.noreply.github.com>
2024-06-01 11:06:07 +0700

    Cb ghi nhan score
    Storage Manager
    Storage Helper
    GameData

---
+++ Assets/Scripts/GameData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Assets.Scripts
+{
+    //luu tru thong tin game
+    [Serializable]
+    public class GameData
+    {
+        public int score = 0;
+        public string timePlayed;
+    }
+
+    [Serializable]
+    public class GameDataPlayed
+    {
+        public List<GameData> plays;
+    }
+}
