8e294f819a421b33c337032256cfc5a5ae5e50eb
LapizLazulie <169118081+LapizLazulie@users.noreply.github.com>
2024-06-04 00:03:12 +0700

    finish

--- Assets/Scripts/Finish.cs
+++ Assets/Scripts/Finish.cs
@@ -13,6 +13,13 @@ public class Finish : MonoBehaviour
     private StorageHelper storageHelper;
     private GameDataPlayed played;
 
+    [SerializeField] GameObject row;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
     private void Start()
     {
         storageHelper = new StorageHelper();
@@ -38,6 +45,21 @@ public class Finish : MonoBehaviour
             storageHelper.LoadData();
             played = storageHelper.played;
             Debug.Log("Count: " + played.plays.Count);
+            // sap xep giam dan theo diem
+            //lay top 5
+            played.plays.Sort((x, y) => y.score.CompareTo(x.score));
+            var plays = played.plays.GetRange(0, Math.Min(5, played.plays.Count));
+
+            //hien thi len giao dien
+            for (int i = 0; i < plays.Count; i++)
+            {
+                var rowInstance = Instantiate(row, row.transform.parent);
+                rowInstance.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = (i + 1).ToString();
+                rowInstance.transform.GetChild(1).GetComponent<TMPro.TextMeshProUGUI>().text = plays[i].score.ToString();
+                rowInstance.transform.GetChild(2).GetComponent<TMPro.TextMeshProUGUI>().text = plays[i].timePlayed;
+                rowInstance.SetActive(true);
+            }
+            //hien thi giao dien ket thuc
             finish.SetActive(true);
         }
     }
