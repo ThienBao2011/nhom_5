5573cd3ba2720bba66acc3b9be0fcafc54cb4a5d
Nguyễn Hải <hainvdps38602@gmail.com>
2024-06-03 23:45:36 +0700

    fix!

--- Assets/Scene4/Scripts/Finish.cs
+++ Assets/Scene4/Scripts/Finish.cs
@@ -18,38 +18,6 @@ public class Finish : MonoBehaviour
         storageHelper.LoadData();
         played = storageHelper.played;
     }
-
-    /*private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.CompareTag("Player")) 
-        {
-            infoCanvas.SetActive(false);
-            var score = FindObjectOfType<GameCotroller>().GetScore();
-            var gameData = new GameData()
-            {
-                score = score,
-                timePlayed = DateTime.Now.ToString("yyyy-MM-dd")
-            };
-            played.plays.Add(gameData);
-            storageHelper.SaveData();
-
-            storageHelper.LoadData();
-            played = storageHelper.played;
-            Debug.Log("Count: " + played.plays.Count);
-
-            played.plays.Sort((x, y) => y.score.CompareTo(x.score));
-            played.plays = played.plays.GetRange(0, Math.Min(5, played.plays.Count));
-            for (int i = 0; i < played.plays.Count; i++)
-            {
-                var rowInstance = Instantiate(row, row.transform.parent);
-                rowInstance.transform.GetChild(0).GetComponent<TMPro.TextMeshProUGUI>().text = (i + 1).ToString();
-                rowInstance.transform.GetChild(1).GetComponent<TMPro.TextMeshProUGUI>().text = played.plays[i].score.ToString();
-                rowInstance.transform.GetChild(2).GetComponent<TMPro.TextMeshProUGUI>().text = played.plays[i].timePlayed;
-                rowInstance.SetActive(true);
-            }
-            finishCanvas.SetActive(true);
-        }
-    }*/
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
